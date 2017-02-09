e984ef6a4cbb8afd7c441b59bd0ed6c8c690c3db
Nan Jiang <jinan@microsoft.com>
2017-02-10 02:08:42 +0800

    Delete a line

--- server.js
+++ server.js
@@ -547,7 +547,6 @@ function sendTextToClassify(recipientId, messageText) {
 
   sendTextMessage(recipientId, res.topScoringIntent.intent);
 
-  // callSendAPI(messageData);
 }
 /*
  * Send a button message using the Send API.
