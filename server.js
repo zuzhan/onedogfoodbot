cd07c9d623737fe044e7433b70c06887027eb7e0
Nan Jiang <jinan@microsoft.com>
2017-02-09 15:33:14 +0800

    Fix bug

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const
   https = require('https'),
   request = require('request');
 const getIntention = require('./LuisAPI');
-const res = getIntention('Go to bed early tonight');
+//const res = getIntention('Go to bed early tonight');
 var app = express();
 var liveConnect = require('./lib/liveconnect-client');
 app.set('port', process.env.PORT || 5000);
@@ -547,7 +547,7 @@ function sendTextToClassify(recipientId, messageText) {
 
   sendTextMessage(recipientId, res.topScoringIntent.intent);
 
-  callSendAPI(messageData);
+  // callSendAPI(messageData);
 }
 /*
  * Send a button message using the Send API.
