dc47e09e34c755459c0e7c857ca22c559721010c
Nan Jiang <jinan@microsoft.com>
2017-02-10 06:23:26 +0800

    Delete post

--- OCRApi.js
+++ OCRApi.js
@@ -1,15 +1,15 @@
 var request = require('request');
 module.exports = function getTextFromImg(senderID, attachments, callback) {
-    var req = request.post('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/489d607e-e0a1-42e4-bc96-2fd9b4049318?subscription-key=77f9c9a858314f308245ec835d6d6091', function (err, resp, body) {
-        if (err) {
-            console.log('Error!');
-        } else {
-            console.log('URL: ' + body);
-        }
+    // var req = request.post('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/489d607e-e0a1-42e4-bc96-2fd9b4049318?subscription-key=77f9c9a858314f308245ec835d6d6091', function (err, resp, body) {
+    //     if (err) {
+    //         console.log('Error!');
+    //     } else {
+    //         console.log('URL: ' + body);
+    //     }
         callback(senderID, '1');
         const text = attachments[0].payload;
         callback(senderID, text);
-    });
+    // });
     // const form = req.form();
     // form.append('file', buffer, {
     //     filename: form,
