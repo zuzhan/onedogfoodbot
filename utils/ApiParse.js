b9b887494e68bd2b4735fb5e1db6eff6b0ad1a17
zuzhan <zuzhanzzm@gmail.com>
2017-02-09 23:32:45 -0800

    ParseNotebooks3

--- utils/ApiParse.js
+++ utils/ApiParse.js
@@ -1,5 +1,9 @@
 var ApiParse = function() {
   this.ParseNotebooks = function(req) {
+      console.log(JSON.stringify(req));
+      console.log(JSON.stringify(req.request));
+      console.log(JSON.stringify(req.request.responseText));
+      console.log(JSON.stringify(req.request.responseText.value));
     return req.request.responseText.value;
   }
 };
