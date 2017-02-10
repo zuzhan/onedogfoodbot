230d01cd2b366a20435ccafec414fae644c7a158
zuzhan <zuzhanzzm@gmail.com>
2017-02-09 19:22:56 -0800

    fuck2

--- server.js
+++ server.js
@@ -945,7 +945,7 @@ app.listen(app.get('port'), function() {
   console.log(liveConnect.getAuthUrl());
   console.log('Node app is running on port', app.get('port'));
   var temp = new onenoteapi.OneNoteApi("fuck", 3600);
-  var promise = temp.getNotebooks(false);
+  var promise = temp.getPages({top: 5});
   console.log(JSON.stringify(typeof promise));
 });
 
