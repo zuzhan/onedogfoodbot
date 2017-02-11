6d2aa91e2cb1d2ca1893d9a51533175682845e40
zuzhan <zuzhanzzm@gmail.com>
2017-02-11 12:58:12 -0800

    upload file 0

--- server.js
+++ server.js
@@ -459,7 +459,7 @@ function editPageAppendImages(recipientId, pageId, attachments) {
 
 /*
  * Delivery Confirmation Event
- *
+ * 
  * This event is sent to confirm the delivery of a message. Read more about
  * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
  *
