03ebc171ad532afb607d054b702cbc945cf9c531
Nan Jiang <jinan@microsoft.com>
2017-02-12 09:21:01 +0800

    check null default section

--- server.js
+++ server.js
@@ -659,9 +659,14 @@ function processOpenNotebookPostback(recipientId, notebookId) {
 }
 
 function openQuickNoteSection(recipientId) {
+  if(!sectionId){
+    console('null section id!');
+    return;
+  }
   Token.GetToken(recipientId).ActiveEditPageId = undefined;
   Token.GetToken(recipientId).ActiveSectionId = undefined;
   var sectionId = Token.getDefaultSectionId(recipientId);
+
   console.log('default section: ' + sectionId);
   var promise = Token.GetToken(recipientId).OneNoteApi.getPages({ sectionId: sectionId });
   promise.then(function (req) {
